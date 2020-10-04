f305b20563b2da154689fea5a2d92c58a130b280
aland1013 <adam.land@gmail.com>
2020-10-04 14:24:50 -0400

    mvp complete / implemented order-agnostic binary search for stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -29,5 +29,25 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
-    pass
-
+    start = 0
+    end = len(arr) - 1
+    isAscending = arr[start] < arr[end]
+    
+    while start <= end:
+        mid = start + (end - start) // 2
+        
+        if target == arr[mid]:
+            return mid
+        
+        if isAscending:
+            if target < arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if target > arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+    
+    return -1
